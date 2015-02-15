18600e7cee37d1125aab5d2877e2f7371133bdfe
YICHUN <YICHUN@E.local>
2015-02-15 18:47:27 +0800

    Export的收據ID取消規則，改流水號

--- TryTravel2gether/Trip+Export.m
+++ TryTravel2gether/Trip+Export.m
@@ -101,27 +101,28 @@
     
     int itemSerial=0;
     //流水號格式：TxDxx，後加上收據在該天的流水號
-    NSString *serTripID=@"T%@";
-    serTripID=[NSString stringWithFormat:serTripID,self.tripIndex];
+    //NSString *serTripID=@"T%@";
+    //serTripID=[NSString stringWithFormat:serTripID,self.tripIndex];
+    int receiptSerial=0;
     for (Day * day in self.days) {
         NSNumberFormatter *numberFormatter=[[NSNumberFormatter alloc]init];
         [numberFormatter setPaddingPosition:NSNumberFormatterPadBeforePrefix];
         [numberFormatter setPaddingCharacter:@"0"];
         [numberFormatter setMinimumIntegerDigits:2];
-        NSString *serDayID=[serTripID stringByAppendingString:[@"D" stringByAppendingString: [numberFormatter stringFromNumber: day.dayIndex]]];//旅遊日流水號
-        int receiptSerial=0;
+        //NSString *serDayID=[serTripID stringByAppendingString:[@"D" stringByAppendingString: [numberFormatter stringFromNumber: day.dayIndex]]];//旅遊日流水號
         
         for (Receipt * receipt in day.receipts) {
             receiptSerial=receiptSerial+1;
+            //取消收據代號格式（因為prepare的dayIndex都=0）
             //收據代號=旅遊日流水號+該日收據流水號
-            NSString *receiptSerID=[serDayID stringByAppendingString:[NSString stringWithFormat:@"R%03i",receiptSerial]];
+            //NSString *receiptSerID=[serDayID stringByAppendingString:[NSString stringWithFormat:@"R%03i",receiptSerial]];
             NSString *thisCurrencySign=receipt.dayCurrency.currency.standardSign;
             NSString *thisReceiptDesc=receipt.desc;
             NSString *thisReceiptDayTime=[NSString stringWithFormat:@"%@ %@",[Trip.dateFormatter_GMT stringFromDate:receipt.day.date] ,[Trip.timeFormatter_GMT stringFromDate:receipt.time]];
             GuyInTrip *payedGuy=[receipt.account.guysInTrip anyObject];
             receiptSavingString=[receiptSavingString stringByAppendingString:
                                  [NSString stringWithFormat:outputReceiptFormat,
-                                  receiptSerID,
+                                  [NSString stringWithFormat:@"R%04i",receiptSerial],
                                   thisReceiptDayTime,
                                   thisReceiptDesc,
                                   thisCurrencySign,
@@ -137,7 +138,7 @@
                 for (GuyInTrip *guyInTrip in item.group.guysInTrip) {
                     itemSavingString=[itemSavingString stringByAppendingString:
                                       [NSString stringWithFormat:outputItemFormat,
-                                       [NSString stringWithFormat:@"R%05i",itemSerial],
+                                       [NSString stringWithFormat:@"I%05i",itemSerial],
                                        thisReceiptDayTime,
                                        item.catInTrip.category.name,
                                        thisReceiptDesc,
@@ -148,7 +149,7 @@
                                        item.totalPrice,
                                        item.group.name,
                                        [NSString stringWithFormat:@"%lu", (unsigned long)item.group.guysInTrip.count],
-                                       receiptSerID,
+                                       [NSString stringWithFormat:@"R%04i",receiptSerial],
                                        guyInTrip.guy.name,
                                        item.sharedPrice
                                        ]
